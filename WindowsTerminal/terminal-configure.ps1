eae0d3c96d0aaedb0f4db87da12e33e060cbcba9
Philip Scheetz <philip@scheetznet.com>
2022-08-17 14:10:28 -0700

    Made the program actually work

--- WindowsTerminal/terminal-configure.ps1
+++ WindowsTerminal/terminal-configure.ps1
@@ -1,19 +1,18 @@
 $env_file = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+Write-Host "Applying Windows Terminal Settings..."
+# Makes a duplicate just in case
 Copy-Item $env_file -Destination "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.old"
+Copy-Item "./settings.json" -Destination $env_file
 
-#Copy-Item "./settings.json" -Destination "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Write-Host "Installing Oh My Posh"
+winget install JanDeDobbeleer.OhMyPosh -s winget
 
-#winget install JanDeDobbeleer.OhMyPosh -s winget
-
-#Write-Host "Copying Oh My Posh - PL10K theme"
-#Copy-Item "./pl10k_customized.omp.json" --Destination "$HOME\.pl10k_customized.omp.json"
-
+Write-Host "Copying Oh My Posh - PL10K theme"
+Copy-Item "./.pl10k_customized.omp.json" -Destination "C:\Users\$env:USERNAME\.pl10k_customized.omp.json"
 
 # Sets the pl10k theme
-#Add-Content -Path $PROFILE -Value "oh-my-posh init pwsh --config ~/.pl10k_customized.omp.json | Invoke-Expression"
-#Get-Content -Path $PROFILE
+Write-Host "Adding Oh My Posh to the PROFILE variable"
+echo "oh-my-posh init pwsh --config ~/.pl10k_customized.omp.json | Invoke-Expression" > $PROFILE
 
-
-# echo "oh-my-posh init pwsh --config ~/.pl10k_customized.omp.json | Invoke-Expression" > $PROFILE
-
-#Write-Host "Done. Restart terminal to apply"
+Write-Host "Done. Restart Windows Terminal to apply"
